91d3794d664497555b0d0307c80589116b7d4d5e
jinny <pjiny92@gmail.com>
2024-07-03 12:23:47 +0900

    refactor: 파일명 수정

--- src/main/java/com/example/companion/controller/CategoryController.java
+++ src/main/java/com/example/companion/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class categoryController {
+public class CategoryController {
 
  @Autowired
  CategoryGoodsListService categoryGoodsListService;
