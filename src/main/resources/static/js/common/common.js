641f95aad43c27d2a4b16aa6b0ba602d4d7c75fe
jinny <pjiny92@gmail.com>
2024-07-08 11:25:52 +0900

    design: 공통

---
+++ src/main/resources/static/js/common/common.js
@@ -0,0 +1,19 @@
+function goBack() {
+    window.history.back();
+}
+
+function goOrderList() {
+    if (auth == null) {
+        location.href = "/login/loginForm";
+    } else {
+        location.href = "/purchase/orderList";
+    }
+};
+
+function goCartList() {
+    if (auth == null) {
+        location.href = "/login/loginForm";
+    } else {
+        location.href = "/purchase/orderList";
+    }
+};
